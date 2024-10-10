b27185ec4a9eb8eac8a1e74f23ceaf5ccf918eed
mirrSo <111701517+xiangxinliao43@users.noreply.github.com>
2024-10-10 19:42:15 +0800

    01-init
    hello saas short link

---
+++ admin/src/main/java/com/miirso/shortlink/admin/AdminApplication.java
@@ -0,0 +1,18 @@
+package com.miirso.shortlink.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Package com.miirso.shortlink.admin
+ * @Author miirso
+ * @Date 2024/10/10 19:36
+ */
+@SpringBootApplication
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+
+}
