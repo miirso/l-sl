2cd62207bc34289046d1947de2965d6efe055a9f
mirrSo <111701517+xiangxinliao43@users.noreply.github.com>
2024-10-14 11:35:01 +0800

    19-gateway-init

---
+++ gateway/src/main/java/com/miirso/shortlink/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.miirso.shortlink.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Package com.miirso.shortlink.gateway
+ * @Author miirso
+ * @Date 2024/10/14 11:31
+ */
+
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
