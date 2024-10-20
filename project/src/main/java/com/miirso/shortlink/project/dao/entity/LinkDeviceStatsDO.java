cf5da08b9151b239ce5d7068189911a45e9adb60
mirrSo <111701517+xiangxinliao43@users.noreply.github.com>
2024-10-20 19:14:25 +0800

    37-monitor-device_type

---
+++ project/src/main/java/com/miirso/shortlink/project/dao/entity/LinkDeviceStatsDO.java
@@ -0,0 +1,57 @@
+package com.miirso.shortlink.project.dao.entity;
+
+/**
+ * @Package com.miirso.shortlink.project.dao.entity
+ * @Author miirso
+ * @Date 2024/10/20 19:11
+ */
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * 访问设备统计访问实体
+ */
+@Data
+@TableName("t_link_device_stats")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LinkDeviceStatsDO extends BaseDO{
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 完整短链接
+     */
+    private String fullShortUrl;
+
+    /**
+     * 分组标识
+     */
+    private String gid;
+
+    /**
+     * 日期
+     */
+    private Date date;
+
+    /**
+     * 访问量
+     */
+    private Integer cnt;
+
+    /**
+     * 浏览器
+     */
+    private String device;
+
+}
--- project/src/main/java/com/miirso/shortlink/project/service/impl/ShortLinkServiceImpl.java
+++ project/src/main/java/com/miirso/shortlink/project/service/impl/ShortLinkServiceImpl.java
@@ -87,6 +87,8 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
 
     private final LinkAccessLogsMapper linkAccessLogsMapper;
 
+    private final LinkDeviceStatsMapper linkDeviceStatsMapper;
+
     @Value("${short-link.stats.locale.amap-key}")
     private String statsLocaleAmapKey;
 
@@ -463,6 +465,20 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
                     .fullShortUrl(fullShortUrl)
                     .build();
             linkAccessLogsMapper.insert(linkAccessLogsDO);
+            // ===link access logs info end===
+
+
+            // ===user device type begin===
+            LinkDeviceStatsDO linkDeviceStatsDO = LinkDeviceStatsDO.builder()
+                    .device(LinkUtil.getDevice(((HttpServletRequest) request)))
+                    .cnt(1)
+                    .gid(gid)
+                    .fullShortUrl(fullShortUrl)
+                    .date(new Date())
+                    .build();
+            linkDeviceStatsMapper.shortLinkDeviceState(linkDeviceStatsDO);
+            // ===user device type end===
+
 
         } catch (Throwable ex) {
             log.error("短链接访问量统计异常", ex);
